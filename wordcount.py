05ebab77477682b0ca8fe8a27dfa5c0aaa7e921f
Maily Ramos <maily.ramos1993@gmail.com>
2020-07-14 16:45:12 +0000

    Word Count completed

--- wordcount.py
+++ wordcount.py
@@ -1,3 +1,8 @@
+import sys
+
+from collections import Counter
+
+# print(sys.argv)
 
 def list_of_words(file):
     word_count = {}
@@ -9,8 +14,16 @@ def list_of_words(file):
         line = line.rstrip()
         words =line.split()
 
+
         for word in words:
+
+            word = word.lower()    
+            word = word.strip("'\",.!?-#$%^&();:_")
+            #is thre another way to strip special characters?
+
             word_count[word] = word_count.get(word, 0) + 1
+
+
 
     return word_count
 
@@ -20,12 +33,40 @@ def print_the_words(word_dict):
     for word, count in word_dict.items():
         print(word, count)
 
-word_count = list_of_words("test.txt")
+final_word_count = list_of_words(sys.argv[1])
 
-print_the_words(word_count)
+print_the_words(final_word_count)
+
+# def list_of_words(file):
+
+#     word_count = []
+
+#     file = open(file)
 
 
+#     for line in file:
 
+#         line = line.strip()
+#         words = line.split()
+
+#         for word in words:
+
+#             word = word.lower()    
+#             word = word.strip("'\",.!?-#$%^&();:_")
+
+#         word_count = word + 1
+
+#     print(word_count)
+
+#     cnt = Counter()
+
+#     for word in word_count:
+#         cnt[word] += 1
+
+    
+
+
+# print(list_of_words(sys.argv[1]))
 
 
 
